8dc3ec79ad71030c5c2dbc973d33ecb938c5200e
feng yuan <140664919+ProfFrugal@users.noreply.github.com>
2023-09-20 16:00:07 -0700

    TestServiceStackText

--- Posts/TestServiceStackText.cs
+++ Posts/TestServiceStackText.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.IO;
 using ServiceStack;
 using ServiceStack.Text;
@@ -16,7 +18,7 @@ namespace FrugalCafe.Posts
             {
                 foreach (char ch in s)
                 {
-                    Console.Write((int) ch);
+                    Console.Write((int)ch);
                     Console.Write(' ');
                 }
 
@@ -32,7 +34,7 @@ namespace FrugalCafe.Posts
             StringWriter writer = new StringWriter();
 
             PerfTest.MeasurePerf(
-                () => 
+                () =>
                 {
                     writer.GetStringBuilder().Clear();
                     writer.Write(text.ToCsvField());
@@ -124,6 +126,77 @@ namespace FrugalCafe.Posts
             }
 
             return result;
+        }
+
+        public static void Write(TextWriter writer, IEnumerable<IDictionary<string, string>> records)
+        {
+            HashSet<string> allKeys = new HashSet<string>();
+            SegmentedList<IDictionary<string, string>> copy = null;
+
+            if (!(records is ICollection<IDictionary<string, string>>))
+                copy = new SegmentedList<IDictionary<string, string>>(256);
+
+            foreach (IDictionary<string, string> record in records)
+            {
+                foreach (var kv in record)
+                    allKeys.Add(kv.Key);
+
+                if (copy != null)
+                    copy.Add(record);
+            }
+            
+            string[] headers = new string[allKeys.Count];
+            allKeys.CopyTo(headers, 0);
+            Array.Sort<string>(headers, StringComparer.Ordinal);
+
+            if (!CsvConfig<Dictionary<string, string>>.OmitHeaders)
+                WriteRow(writer, headers);
+
+            if (copy != null)
+                records = copy;
+
+            foreach (IDictionary<string, string> record in records)
+            {
+                for (int i = 0; i < headers.Length; i++)
+                {
+                    if (i != 0)
+                        writer.Write(',');
+
+                    record.TryGetValue(headers[i], out string value);
+                    WriteCsvField(writer, value);
+                }
+
+                writer.WriteLine();
+            }
+        }
+
+        public static void WriteRow(TextWriter writer, IEnumerable<string> row)
+        {
+            if (writer == null)
+            {
+                return;
+            }
+            bool ranOnce = false;
+            
+            foreach (string field in row)
+            {
+                WriteItemSeperatorIfRanOnce(writer, ref ranOnce);
+                writer.Write(field.ToCsvField());
+            }
+            
+            writer.Write(CsvConfig.RowSeparatorString);
+        }
+
+        internal static void WriteItemSeperatorIfRanOnce(TextWriter writer, ref bool ranOnce)
+        {
+            if (ranOnce)
+            {
+                writer.Write(CsvConfig.ItemSeperatorString);
+            }
+            else
+            {
+                ranOnce = true;
+            }
         }
     }
 }
