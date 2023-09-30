c2280d3e6db1c2733db6cd5bab4020bb1176821c
feng yuan <140664919+ProfFrugal@users.noreply.github.com>
2023-09-30 08:45:34 -0700

    SimpleDictionary

--- Common/SimpleDictionary.cs
+++ Common/SimpleDictionary.cs
@@ -19,93 +19,35 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 namespace FrugalCafe
 {
-    public class SmallDictionary<K, V> where K : IEquatable<K>
+    public class SimpleDictionary<K, V>
     {
-        [StructLayout(LayoutKind.Auto)]
         private struct Slot
         {
             internal K key;
             internal V value;
-            internal short next;
+            internal int hashCode;
+            internal int next;
         }
 
-        short[] _buckets;
+        IEqualityComparer<K> _comparer;
+        int[] _buckets;
         Slot[] _slots;
-        ulong _fastModMultiplier;
 
         int _count;
+        int _freeList;
         
-        public SmallDictionary(int capacity = 0)
+        public SimpleDictionary(IEqualityComparer<K> comparer = null)
         {
-            if (capacity > 0)
-            {
-                int size = HashHelpers.GetPrimeAll(capacity);
-                
-                _buckets = new short[size];
-                _slots = new Slot[size];
-                _fastModMultiplier = HashHelpers.GetFastModMultiplier((uint)size);
-            }
-        }
-
-        public SmallDictionary(SmallDictionary<K, V> other)
-        {
-            int size = HashHelpers.GetPrimeAll(other._count);
-
-            _buckets = new short[size];
-            _slots = new Slot[size];
-            _fastModMultiplier = HashHelpers.GetFastModMultiplier((uint)size);
-
-            for (int i = 0; i < other._count; i++)
-            {
-                Add(other._slots[i].key, other._slots[i].value);
-            }
-        }
-
-        public void Clear(bool clearData = true)
-        {
-            if (_count != 0)
-            {
-                Array.Clear(_buckets, 0, _buckets.Length);
-
-                if (clearData)
-                {
-                    Array.Clear(_slots, 0, _count);
-                }
-
-                _count = 0;
-            }
+            _comparer = comparer ?? EqualityComparer<K>.Default;
+            _freeList = -1;
         }
 
         public int Count => _count;
 
         public int Capacity => (_buckets != null) ? _buckets.Length : 0;
-
-        public V this[K key]
-        {
-            get
-            {
-                if (_count > 0)
-                {
-                    int slot = FindSlot(key, InternalGetHashCode(key));
-
-                    if (slot >= 0)
-                    {
-                        return _slots[slot].value;
-                    }
-                }
-
-                throw new KeyNotFoundException();
-            }
-
-            set
-            {
-                TryAdd(key, value, InsertionBehavior.OverwriteExisting);
-            }
-        }
 
         public void Add(K key, V value)
         {
@@ -114,20 +56,17 @@ namespace FrugalCafe
 
         public bool TryAdd(K key, V value, InsertionBehavior whenFound)
         {
-            uint hashCode = InternalGetHashCode(key);
+            int hashCode = InternalGetHashCode(key);
 
-            if (_slots == null)
-            {
-                Resize();
-            }
-
-            int bucket = GetBucket(hashCode);
+            int bucket = 0;
 
             if (_count > 0)
             {
-                for (int i = _buckets[bucket] - 1; i >= 0; i = _slots[i].next)
-                { 
-                    if (_slots[i].key.Equals(key))
+                bucket = hashCode % _buckets.Length;
+
+                for (int i = bucket - 1; i >= 0; i = _slots[i].next)
+                {
+                    if (_slots[i].hashCode == hashCode && _comparer.Equals(_slots[i].key, key))
                     {
                         if (whenFound == InsertionBehavior.ThrowOnExisting)
                         {
@@ -145,25 +84,36 @@ namespace FrugalCafe
                 }
             }
 
-            if (_count == _slots.Length)
-            {
-                Resize();
-                bucket = GetBucket(hashCode);
-            }
+            int index;
 
-            int index = _count;
-            _count++;
+            if (_freeList >= 0)
+            {
+                index = _freeList;
+                _freeList = _slots[index].next;
+            }
+            else
+            {
+                if ((_slots == null) || (_count == _slots.Length))
+                {
+                    Resize();
+                    bucket = hashCode % _buckets.Length;
+                }
+
+                index = _count;
+                _count++;
+            }
 
             _slots[index].key = key;
             _slots[index].value = value;
-            _slots[index].next = (short)(_buckets[bucket] - 1);
+            _slots[index].hashCode = hashCode;
+            _slots[index].next = _buckets[bucket] - 1;
 
-            _buckets[bucket] = (short)(index + 1);
+            _buckets[bucket] = index + 1;
 
             return true;
         }
 
-        public bool ContainsKey(K key)
+        public bool Contains(K key)
         {
             return (_count > 0) && (FindSlot(key, InternalGetHashCode(key)) >= 0);
         }
@@ -187,12 +137,48 @@ namespace FrugalCafe
             return false;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private int FindSlot(K key, uint hashCode)
+        public bool Remove(K key)
         {
-            for (int i = _buckets[GetBucket(hashCode)] - 1; i >= 0; i = _slots[i].next)
+            if (_count > 0)
             {
-                if (_slots[i].key.Equals(key))
+                int hashCode = InternalGetHashCode(key);
+
+                int bucket = hashCode % _buckets.Length;
+                int last = -1;
+
+                for (int i = _buckets[bucket] - 1; i >= 0; last = i, i = _slots[i].next)
+                {
+                    if ((_slots[i].hashCode == hashCode) && _comparer.Equals(_slots[i].key, key))
+                    {
+                        if (last < 0)
+                        {
+                            _buckets[bucket] = _slots[i].next + 1;
+                        }
+                        else
+                        {
+                            _slots[last].next = _slots[i].next;
+                        }
+
+                        _slots[i].key = default(K);
+                        _slots[i].value = default(V);
+                        _slots[i].hashCode = -1;
+                        _slots[i].next = _freeList;
+                        _freeList = i;
+
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private int FindSlot(K key, int hashCode)
+        {
+            for (int i = _buckets[hashCode % _buckets.Length] - 1; i >= 0; i = _slots[i].next)
+            {
+                if (_slots[i].hashCode == hashCode && _comparer.Equals(_slots[i].key, key))
                 {
                     return i;
                 }
@@ -205,41 +191,34 @@ namespace FrugalCafe
         {
             if (_slots == null)
             {
-                _buckets = new short[2];
+                _buckets = new int[2];
                 _slots = new Slot[2];
-                _fastModMultiplier = HashHelpers.GetFastModMultiplier(2);
+
                 return;
             }
 
-            int newSize = HashHelpers.GetPrimeAll(_count * 2 + 1);
+            int newSize = HashHelpers.GetPrime(_count * 2 + 1, HashHelpers.Primes24);
 
+            int[] newBuckets = new int[newSize];
             Slot[] newSlots = new Slot[newSize];
-            _fastModMultiplier = HashHelpers.GetFastModMultiplier((uint)newSize);
-
+            
             Array.Copy(_slots, 0, newSlots, 0, _count);
-
-            _buckets = new short[newSize];
-
+            
             for (int i = 0; i < _count; i++)
             {
-                int bucket = GetBucket(InternalGetHashCode(newSlots[i].key));
-                newSlots[i].next = (short)(_buckets[bucket] - 1);
-                _buckets[bucket] = (short)(i + 1);
+                int bucket = newSlots[i].hashCode % newSize;
+                newSlots[i].next = newBuckets[bucket] - 1;
+                newBuckets[bucket] = i + 1;
             }
             
+            _buckets = newBuckets;
             _slots = newSlots;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private int GetBucket(uint hashCode)
+        private int InternalGetHashCode(K value)
         {
-            return (int)HashHelpers.FastMod(hashCode, (uint)_buckets.Length, _fastModMultiplier);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private uint InternalGetHashCode(K value)
-        {
-            return (uint)value.GetHashCode();
+            return _comparer.GetHashCode(value) & 0x7FFFFFFF;
         }
     }
 }
